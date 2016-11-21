2d2bb33dfa215ad3a5fc9dea66199fb431e3a004
Emile <emile.swarts123@gmail.com>
2016-11-21 18:58:59 +0000

    Moar

--- src/conways_gol/core.clj
+++ src/conways_gol/core.clj
@@ -18,3 +18,33 @@
 (defn tick-cell
   [cell]
   (assoc cell :state (cell-state cell)))
+
+(defn x-rows
+  [world-dimensions]
+  (map (fn [x-row] (hash-map :x x-row)) (take (world-dimensions :x) (range))))
+
+(defn y-rows-from-x-rows
+  [x-row world-dimensions]
+  (fn [x-row]
+    (map
+      (fn [y] (assoc x-row :y y))
+      (take (world-dimensions :y) (range)))))
+
+(defn with-neighbors
+  [cells]
+  cells)
+
+(defn with-existing-cells
+  [generated-cells defined-cells]
+  (with-neighbors
+    (map (fn [cell]
+         (if (not= nil #(filter (fn [cell-row] (and (= (cell-row :x) (= (cell-row :y)))) [(cell :x) (cell :y)]) defined-cells))
+               (assoc cell :state (cell :state))
+               (assoc cell :state 0))) defined-cells)))
+
+(defn rendered-world
+  [defined-cells world-dimensions]
+  (with-existing-cells (y-rows-from-x-rows (x-rows world-dimensions) world-dimensions) defined-cells))
+
+; (def world-dimensions
+;   {:x 9 :y 9})
--- test/conways_gol/core_test.clj
+++ test/conways_gol/core_test.clj
@@ -15,3 +15,15 @@
   (testing "More than 3 neighbors"
     (let [cell { :x 0 :y 0 :state 1 :neighbors [1 1 1 1]}]
     (is (= 0 ((tick-cell cell) :state))))))
+
+(deftest rendered-world-test
+  (testing "Render world grid"
+    (let [cells [{ :x 0 :y 0 :state 1}]
+          world-dimensions {:x 5 :y 5}
+          expected-result [
+                           { :x 0 :y 0 :neighbors [0 0 0] :state 1}
+                           { :x 1 :y 0 :neighbors [1 0 0 0 0] :state 0}
+                           { :x 0 :y 1 :neighbors [1 0 0 0 0] :state 0}
+                           { :x 1 :y 1 :neighbors [1 0 0 0 0 0 0 0] :state 0}
+                           ]]
+      (is (= expected-result (rendered-world cells world-dimensions))))))
