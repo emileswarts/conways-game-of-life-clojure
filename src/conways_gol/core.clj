1175f70736abec97b992b821ba5294d559bb33ad
Emile <emile.swarts123@gmail.com>
2016-11-25 18:56:46 +0000

    It's alive!

--- src/conways_gol/core.clj
+++ src/conways_gol/core.clj
@@ -1,22 +1,33 @@
 (ns conways-gol.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
 (defn state-from-neighbours
-  [live-neighbor-count]
+  [live-neighbour-count]
   (cond
-    (< live-neighbor-count 2) 0
-    (= live-neighbor-count 2) 1
-    (= live-neighbor-count 3) 1
-    (> live-neighbor-count 3) 0))
+    (< live-neighbour-count 2) 0
+    (= live-neighbour-count 2) 1
+    (= live-neighbour-count 3) 1
+    (> live-neighbour-count 3) 0))
 
 (defn cell-state
   [cell cell-neighbours]
   (state-from-neighbours (count (filter #(= (%1 :state) 1) cell-neighbours))))
+
+(defn neighbour?
+  [potential-neighbour-cell cell]
+  (and
+    (and
+      (or (= (cell :x) (potential-neighbour-cell :x))
+          (= (cell :x) (- (potential-neighbour-cell :x) 1))
+          (= (cell :x) (+ (potential-neighbour-cell :x) 1)))
+      (or (= (cell :y) (potential-neighbour-cell :y))
+          (= (cell :y) (- (potential-neighbour-cell :y) 1))
+          (= (cell :y) (+ (potential-neighbour-cell :y) 1))))
+    (not= cell potential-neighbour-cell)))
+
+(defn neighbours
+  [grid cell]
+  (filter #(neighbour? %1 cell) grid))
 
 (defn tick-cell
   [cell grid]
@@ -38,24 +49,23 @@
              (assoc generated-cell :state ((first row) :state))
              (assoc generated-cell :state 0)))) (flatten generated-cells)))
 
-(defn neighbour?
-  [potential-neighbour-cell cell]
-  (and (or
-    (= (cell :x) (potential-neighbour-cell :x))
-    (= (cell :y) (potential-neighbour-cell :y))
-    (= (cell :x) (- (potential-neighbour-cell :x) 1))
-    (= (cell :y) (- (potential-neighbour-cell :y) 1))
-    (= (cell :x) (+ (potential-neighbour-cell :x) 1))
-    (= (cell :y) (+ (potential-neighbour-cell :y) 1)))
-    (not= cell potential-neighbour-cell)))
-
-(defn neighbours
-  [grid cell]
-  (filter #(neighbour? %1 cell) grid))
-
 (defn rendered-world
   [defined-cells world-dimensions]
   (with-existing-cells (map (fn [x-row]
          (map
            (fn [y] (assoc x-row :y y))
            (take (world-dimensions :y) (range)))) (x-rows world-dimensions)) defined-cells))
+
+(defn presentational
+  [world]
+  (println (map #(if (= 1 (% :state)) "*" " ") world))
+  (Thread/sleep 1000)
+  (presentational (rendered-world (map #(tick-cell %1 world) world) {:x 8 :y 8})))
+
+(defn -main [& args]
+  (let [world (rendered-world [{:x 3 :y 2 :state 1}
+                               {:x 4 :y 3 :state 1}
+                               {:x 4 :y 4 :state 1}
+                               {:x 3 :y 4 :state 1}
+                               {:x 2 :y 4 :state 1}] {:x 8 :y 8})]
+    (presentational world)))
